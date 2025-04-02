c593d1a143e9662d1347df0e84c9c9468bcfd9dd
jason Drane <jddevops@dranefamily.com>
2025-04-02 09:28:36 -0400

    updated file functions to use with

--- main.py
+++ main.py
@@ -4,26 +4,25 @@ while True:
     match user_action:
         case "add":
             todo = input("Enter a todo: ") + "\n"
-            file =open('data/todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
-            todos.append(todo)
-            file = open('data/todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
+            with open('data/todos.txt', 'r') as file:
+                todos = file.readlines()
+            with open('data/todos.txt', 'w') as file:
+                todos.append(todo)
+                file.writelines(todos)
+
 
         case "show" | "display":
-            file = open('data/todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+            with open('data/todos.txt', 'r') as file:
+                todos = file.readlines()
+
             current_todos = [item.title().strip('\n') for item in todos]
             for index, item in enumerate(current_todos):
                 print(f"{index + 1}.{item}")
 
         case "edit":
             file = open('data/todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+            with open('data/todos.txt', 'r') as file:
+                todos = file.readlines()
             for index, item in enumerate(todos):
                 item = item.strip('\n').title()
                 print(f"{index + 1}.{item}")
@@ -31,23 +30,21 @@ while True:
             number = number -1
             new_todo = input("Enter new todo item: ")
             todos[number] = new_todo
-            file = open('data/todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
+            with open('data/todos.txt', 'w') as file:
+                file.writelines(todos)
+
 
         case "complete":
-            file = open('data/todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+            with open('data/todos.txt', 'r') as file:
+                todos = file.readlines()
             for index, item in enumerate(todos):
                 item = item.strip('\n').title()
                 print(f"{index + 1}.{item}")
             number = int(input("Number of todo to complete: "))
             print(f"{todos[number-1]} was removed from the list.")
             todos.pop(number -1)
-            file = open('data/todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
+            with open('data/todos.txt', 'w') as file:
+                file.writelines(todos)
 
         case "exit":
             break
