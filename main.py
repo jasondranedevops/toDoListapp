55c4d04a5a5d0f7fda84710344f4c6a4b4129a7a
jason Drane <jddevops@dranefamily.com>
2025-04-02 10:55:12 -0400

    replaced match case with if statments to allow add fuction to include added item

--- main.py
+++ main.py
@@ -1,53 +1,54 @@
 while True:
     user_action = input("Type add, show, edit, complete or exit: ").strip()
 
-    match user_action:
-        case "add":
-            todo = input("Enter a todo: ") + "\n"
-            with open('data/todos.txt', 'r') as file:
-                todos = file.readlines()
-            with open('data/todos.txt', 'w') as file:
-                todos.append(todo)
-                file.writelines(todos)
+    # Check for valid user input
+    valid_actions = ['add', 'show', 'edit', 'complete', 'exit']
 
-        case "show" | "display":
-            with open('data/todos.txt', 'r') as file:
-                todos = file.readlines()
+    if "add" in user_action:
+        todo = user_action[4:] + "\n"
+        with open('data/todos.txt', 'r') as file:
+            todos = file.readlines()
+        with open('data/todos.txt', 'w') as file:
+            todos.append(todo)
+            file.writelines(todos)
 
-            current_todos = [item.title().strip('\n') for item in todos]
-            for index, item in enumerate(current_todos):
-                print(f"{index + 1}.{item}")
+    elif "show" in user_action or "display" in user_action:
+        with open('data/todos.txt', 'r') as file:
+            todos = file.readlines()
 
-        case "edit":
-            file = open('data/todos.txt', 'r')
-            with open('data/todos.txt', 'r') as file:
-                todos = file.readlines()
-            for index, item in enumerate(todos):
-                item = item.strip('\n').title()
-                print(f"{index + 1}.{item}")
-            number = int(input("Number of todo to edit: "))
-            number = number -1
-            new_todo = input("Enter new todo item: ")
-            todos[number] = new_todo
-            with open('data/todos.txt', 'w') as file:
-                file.writelines(todos)
+        current_todos = [item.title().strip('\n') for item in todos]
+        for index, item in enumerate(current_todos):
+            print(f"{index + 1}.{item}")
 
-        case "complete":
-            with open('data/todos.txt', 'r') as file:
-                todos = file.readlines()
-            for index, item in enumerate(todos):
-                item = item.strip('\n').title()
-                print(f"{index + 1}.{item}")
-            number = int(input("Number of todo to complete: "))
-            print(f"{todos[number-1]} was removed from the list.")
-            todos.pop(number -1)
-            with open('data/todos.txt', 'w') as file:
-                file.writelines(todos)
+    elif "edit" in user_action:
+        with open('data/todos.txt', 'r') as file:
+            todos = file.readlines()
+        for index, item in enumerate(todos):
+            item = item.strip('\n').title()
+            print(f"{index + 1}.{item}")
+        number = int(input("Number of todo to edit: "))
+        number = number - 1
+        new_todo = input("Enter new todo item: ")
+        todos[number] = new_todo
+        with open('data/todos.txt', 'w') as file:
+            file.writelines(todos)
 
-        case "exit":
-            break
-        case _:
-            print("pleas use only add, show, edit, complete or exit")
+    elif "complete" in user_action:
+        with open('data/todos.txt', 'r') as file:
+            todos = file.readlines()
+        for index, item in enumerate(todos):
+            item = item.strip('\n').title()
+            print(f"{index + 1}.{item}")
+        number = int(input("Number of todo to complete: "))
+        print(f"{todos[number-1]} was removed from the list.")
+        todos.pop(number - 1)
+        with open('data/todos.txt', 'w') as file:
+            file.writelines(todos)
+
+    elif "exit" in user_action:
+        break
+
+    else:
+        print("Please use only add, show, edit, complete or exit")
 
 print("See you soon!")
-
