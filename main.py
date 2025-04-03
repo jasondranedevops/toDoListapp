19820922317cd12dc39c91da8f1752f81f540d05
jason Drane <jddevops@dranefamily.com>
2025-04-02 20:05:34 -0400

    replace redundant code with functions

--- main.py
+++ main.py
@@ -1,47 +1,55 @@
+def get_todos():
+    with open('data/todos.txt', 'r') as file:
+        todos = file.readlines()
+    return todos
+
+def write_todos():
+    with open('data/todos.txt', 'w') as file:
+        todos.append(todo + "\n")
+        file.writelines(todos)
+    return todos
+
+def edit_todo():
+    todos[number] = new_todo + '\n'
+    with open('data/todos.txt', 'w') as file:
+        file.writelines(todos)
+    return todos
+
+def list_todos():
+    current_todos = [item.title().strip('\n') for item in todos]
+    for index, item in enumerate(current_todos):
+        print(f"{index + 1}.{item}")
+    return todos
+
+
 while True:
     user_action = input("Type new or add, show or display, edit, complete or done or exit or 'close: ").strip()
 
     # Check for valid user input
-    valid_actions = ['new', 'add', 'display', 'show', 'done', 'edit', 'complete', 'exit', 'close']
-
+    #valid_actions = ['new', 'add', 'display', 'show', 'done', 'edit', 'complete', 'exit', 'close']
     if user_action.startswith("add") or user_action.startswith("new"):
-        with open('data/todos.txt', 'r') as file:
-            todos = file.readlines()
-
+        todos = get_todos()
         if len(user_action) > 4:
             todo = user_action[4:]
-            with open('data/todos.txt', 'w') as file:
-                todos.append(todo + "\n")
-                file.writelines(todos)
+            todos = write_todos()
         else:
             todo = input("Enter a todo: ")
-            with open('data/todos.txt', 'w') as file:
-                todos.append(todo + "\n")
-                file.writelines(todos)
-
+            todos = write_todos()
         print(f"{todo} added to list.")
 
     elif user_action.startswith("show") or user_action.startswith("display"):
-        with open('data/todos.txt', 'r') as file:
-            todos = file.readlines()
-        current_todos = [item.title().strip('\n') for item in todos]
-        for index, item in enumerate(current_todos):
-            print(f"{index + 1}.{item}")
+        todos = get_todos()
+        todos = list_todos()
 
     elif user_action.startswith("edit"):
         if len(user_action) > 5:
             number = int(user_action[5:]) -1
             new_todo = input("Enter new todo item: ")
-            todos[number] = new_todo + "\n"
-            with open('data/todos.txt', 'w') as file:
-                file.writelines(todos)
+            todos = edit_todo()
             print(f"To Do list item numbered {number + 1}. updated to {new_todo}")
         else:
-            with open('data/todos.txt', 'r') as file:
-                todos = file.readlines()
-            for index, item in enumerate(todos):
-                item = item.strip('\n').title()
-                print(f"{index + 1}.{item}")
+            todos = get_todos()
+            todos = list_todos()
             number = int(input("Number of todo to edit: "))
             if number > len(todos) or number < 1:
                 print(f"To Do list does not contain and item numbered {number}. Try again")
@@ -49,18 +57,13 @@ while True:
             else:
                 number = number - 1
                 new_todo = input("Enter new todo item: ")
-                todos[number] = new_todo + '\n'
-                with open('data/todos.txt', 'w') as file:
-                    file.writelines(todos)
+                todos = edit_todo()
                 print(f"To Do list item numbered {number + 1}. updated to {new_todo}")
 
 
     elif user_action.startswith("complete") or user_action.startswith("done"):
-        with open('data/todos.txt', 'r') as file:
-            todos = file.readlines()
-        for index, item in enumerate(todos):
-            item = item.strip('\n').title()
-            print(f"{index + 1}.{item}")
+        todos = get_todos()
+        todos = list_todos()
         number = int(input("Number of todo to complete: "))
         try:
             print(f"{todos[number-1]} was removed from the list.")
